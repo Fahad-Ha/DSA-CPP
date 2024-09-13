2710c0679e3df764655417418f297e5f0d20d497
Fahad-Ha <fahhod.ahmad@gmail.com>
2024-09-13 18:02:00 +0300

    queue added enqueue method

--- queue.cpp
+++ queue.cpp
@@ -38,6 +38,18 @@ public:
         }
     }
 
+    void enqueue(int value) {
+        Node* newNode = new Node(value);
+        if (length == 0) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last->next = newNode;
+            last = newNode;
+        }
+        length++;
+    }
+
 
     void getFirst() {
         cout << "First: " << first->value << endl;
