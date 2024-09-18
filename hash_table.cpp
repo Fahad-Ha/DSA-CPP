a508fee53cf7c0006c85ec20b238c577621d8417
Fahad-Ha <fahhod.ahmad@gmail.com>
2024-09-18 20:21:52 +0300

    added hash table

---
+++ hash_table.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+#include <string> // Include string header
+using namespace std;
+
+class Node {
+    public:
+        string key;
+        int value;
+        Node* next;
+
+        Node(string key,int value) {
+            this->key = key;
+            this->value = value;
+            next = nullptr;
+        }
+
+};
+
+class HashTable {
+    private:
+        // Used 7 because a prime number will have less collisions
+        static const int SIZE = 7;
+        Node* dataMap[SIZE];
+
+    public:
+        int hash(string key) {
+            int hash = 0;
+            for (int i = 0; i < key.length(); i++) {
+                int asciiValue = int(key[i]);
+                // Used 23 because 23 * prime number will be more random
+                hash = (hash + asciiValue * 23) % SIZE;
+            }
+            return hash;
+        }
+};
+
+
+int main()
+{
+    HashTable* myHashTable = new HashTable();
+
+    cout << "Hash: " << myHashTable->hash("test");
+
+}
